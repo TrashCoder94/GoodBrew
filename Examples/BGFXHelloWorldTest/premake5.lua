94fe4b3016c4e7c7b7188a7068ec28f06a2c5dd0
TrashCoder94 <jmottershead1994@gmail.com>
2023-02-12 15:10:19 +0000

    Update premake5.lua
    The hello world example doesn't use any of the engine files, so removing that as an include/link.
    
    Manually linking against bgfx/ImGui

--- Examples/BGFXHelloWorldTest/premake5.lua
+++ Examples/BGFXHelloWorldTest/premake5.lua
@@ -27,5 +27,14 @@ project "BGFXHelloWorldTest"
 		"%{IncludeDir.imgui}"
 	}
 
-	links { "GBEngine" }
+	links { "bgfx", "bimg", "bx", "glfw", "ImGui" }
+
+	filter "system:windows"
+		links { "gdi32", "kernel32", "psapi" }
+	
+	filter "system:linux"
+		links { "dl", "GL", "pthread", "X11" }
+		
+	filter "system:macosx"
+		links { "QuartzCore.framework", "Metal.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework", "CoreGraphics.framework" }
 	setBxCompat()

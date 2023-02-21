6b575eec64e3c98686917a941bab84b47b0f8294
TrashCoder94 <jmottershead1994@gmail.com>
2023-02-21 18:27:48 +0000

    Update: Build Machine Scripts
    Moving directory to see if I can just build the "Shared" project Makefile and then change back to the root directory and perform the rest of the operations needed.

--- Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Mac-Build-Debug.sh
@@ -9,14 +9,18 @@ echo "Generating project files failed, exiting out..."
 exit 1
 fi
 
+cd /ThirdParty/Shared
+pwd
 echo "Generating binaries for ThirdParty dependencies..."
-make config=debug_x86_64 project=Shared
+make config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building ThirdParty dependencies failed, exiting out..."
 exit 1
 fi
 
+cd ../../
+pwd
 echo "Linking binaries for ThirdParty dependencies..."
 ThirdParty/premake/Binaries/premake5_mac --file=premake5.lua gmake2
 if [ $? -ne 0 ] 

0a1d0cafe1c114d66cc4c419791a34353159b195
TrashCoder94 <jmottershead1994@gmail.com>
2023-02-27 17:35:53 +0000

    Update BuildMachine-Linux-Build-Debug.sh
    Just trying to build Shared...

--- Scripts/BuildMachine/BuildMachine-Linux-Build-Debug.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build-Debug.sh
@@ -10,11 +10,7 @@ exit 1
 fi
 
 echo "Generating binaries for ThirdParty dependencies..."
-sudo chmod o+x ./ThirdParty/Shared
-cd ./ThirdParty/Shared
-pwd
-echo "About to try and just make the Shared project"
-make Shared --always-make config=debug_x86_64
+make Shared config=debug_x86_64
 if [ $? -ne 0 ] 
 then
 echo "Building ThirdParty dependencies failed, exiting out..."
@@ -22,23 +18,23 @@ echo "Building ThirdParty dependencies failed, exiting out..."
 exit 1
 fi
 
-echo "Linking binaries for ThirdParty dependencies..."
-cd ../../
-pwd
-#find . -type f
-ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
-if [ $? -ne 0 ] 
-then
-echo "Linking binaries for ThirdParty dependencies failed, exiting out..."
-#find . -type f
-exit 1
-fi
+#echo "Linking binaries for ThirdParty dependencies..."
+#cd ../../
+#pwd
+##find . -type f
+#ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
+#if [ $? -ne 0 ] 
+#then
+#echo "Linking binaries for ThirdParty dependencies failed, exiting out..."
+##find . -type f
+#exit 1
+#fi
 
-echo "Building Debug Configuration"
-make config=debug_x86_64
-if [ $? -ne 0 ] 
-then
-echo "Building Debug Configuration failed, exiting out..."
-#find . -type f
-exit 1
-fi
+#echo "Building Debug Configuration"
+#make config=debug_x86_64
+#if [ $? -ne 0 ] 
+#then
+#echo "Building Debug Configuration failed, exiting out..."
+##find . -type f
+#exit 1
+#fi

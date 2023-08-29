17094c4f7d68a6c79a47543571c5fdff9cbb0cfe
TrashCoder94 <jmottershead1994@gmail.com>
2023-08-29 20:15:26 +0100

    Update BuildMachine-Linux-Run-Release.sh
    :penguin: - Added './' in front of the binary executable name

--- Scripts/BuildMachine/BuildMachine-Linux-Run-Release.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Run-Release.sh
@@ -4,10 +4,9 @@ echo "Running Release BootupAndShutdownTest executable!"
 chmod o+rx Binaries/Release-linux-x86_64/BootupAndShutdownTest/Assets
 chmod +x Binaries/Release-linux-x86_64/BootupAndShutdownTest/BootupAndShutdownTest
 pwd
-sudo cd Binaries/Release-linux-x86_64/BootupAndShutdownTest
-find . -type f
+cd Binaries/Release-linux-x86_64/BootupAndShutdownTest
 pwd
-BootupAndShutdownTest
+./BootupAndShutdownTest
 if [ $? -ne 0 ] 
 then
 echo "Running release BootupAndShutdownTest executable failed, exiting out..."
